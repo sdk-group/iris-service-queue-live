79ecad4cbc05389ab6397871bfa5b59c06cdd0db
YuuKohaku <soul.of.bytes@gmail.com>
2016-11-03 17:12:30 +0700

    service name

--- src/queue-live/index.js
+++ src/queue-live/index.js
@@ -9,11 +9,11 @@ let tasks = [];
 
 module.exports = {
 	module: require('./queue-live.js'),
+	name: 'queue-live',
 	permissions: [],
 	exposed: true,
 	tasks: tasks,
 	events: {
 		group: 'queue-live',
-		shorthands: events.queue-live_box
 	}
 };
